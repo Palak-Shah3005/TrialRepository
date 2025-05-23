ae4f5d9142d1cd1c3179353efd4c6f297b90bd31
Palak Shah <palakshah3005@gmail.com>
2025-05-23 21:50:34 +0530

    Changes from third branch

--- sample.py
+++ sample.py
@@ -1,11 +1,10 @@
 def square_number(num):
-    return num ** 2
+    return num * 2
 
 def main():
     user_input = float(input("Enter a number: "))
     result = square_number(user_input)
     print("The square of the number is:", result)
-    print("This change was made in first branch.")
-    print("This was another change made in forst branch")
+    print("Change through third branch")
 # Run the program
 main()
