cbea1163f6794ce9d962ffbba372dd54376ff1ce
Palak Shah <palakshah3005@gmail.com>
2025-05-23 21:13:31 +0530

    New commit from first_branch

--- sample.py
+++ sample.py
@@ -5,6 +5,7 @@ def main():
     user_input = float(input("Enter a number: "))
     result = square_number(user_input)
     print("The square of the number is:", result)
-
+    print("This change was made in first branch.")
+    print("This was another change made in forst branch")
 # Run the program
 main()
